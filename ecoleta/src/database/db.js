bffd2d343cedac77fc12a69dd5fa179a29308e3b
W. Guilherme <wguilhermecriativo@gmail.com>
2022-05-03 16:08:50 -0300

    update

--- ecoleta/src/database/db.js
+++ ecoleta/src/database/db.js
@@ -4,60 +4,77 @@ const sqlite3 = require("sqlite3").verbose();
 // init db object
 const db = new sqlite3.Database("./src/database/database.db");
 
-// use db object for our operations
-db.serialize(() => {
-  // with vanilla SQL
+// // use db object for our operations
+// db.serialize(() => {
+//   // with vanilla SQL
 
-  // create table
-  db.run(`
-      CREATE TABLE IF NOT EXISTS places (
-         id INTEGER PRIMARY KEY AUTOINCREMENT,
-         image TEXT,
-         name TEXT,
-         address TEXT,
-         address2 TEXT,
-         state TEXT,
-         city TEXT,
-         items TEXT 
-      );
-   `);
+//   // create table
+//   db.run(`
+//       CREATE TABLE IF NOT EXISTS places (
+//          id INTEGER PRIMARY KEY AUTOINCREMENT,
+//          image TEXT,
+//          name TEXT,
+//          address TEXT,
+//          address2 TEXT,
+//          state TEXT,
+//          city TEXT,
+//          items TEXT 
+//       );
+//    `);
 
-  // insert data
+//   // insert data
 
-  const query = `
-   INSERT INTO places (
-      image,
-      name,
-      address,
-      address2,
-      state,
-      city,
-      items
+//   const query = `
+//    INSERT INTO places (
+//       image,
+//       name,
+//       address,
+//       address2,
+//       state,
+//       city,
+//       items
 
-   ) VALUES (?,?,?,?,?,?,?)
-`;
+//    ) VALUES (?,?,?,?,?,?,?)
+// `;
 
-  const values = [
-    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fblog.castellmaq.com.br%2Fwp-content%2Fuploads%2F2018%2F04%2F184429-5-dicas-para-reaproveitar-reciclar-embalagens-e-reduzir-perdas-1280x640.jpg&imgrefurl=https%3A%2F%2Fblog.castellmaq.com.br%2F5-dicas-para-reaproveitar-reciclar-embalagens-e-reduzir-perdas%2F&tbnid=Sy8_K7odN6P4uM&vet=12ahUKEwi-xZHd3e7qAhX8BbkGHT7fAzIQMygGegUIARDeAQ..i&docid=KvBOy9wcjhs3PM&w=1280&h=640&q=recicilclar&ved=2ahUKEwi-xZHd3e7qAhX8BbkGHT7fAzIQMygGegUIARDeAQ",
-    "Colectoria",
-    "Guilherme Gemballa, Jardim América",
-    "Número 233",
-    "Santa Catarina",
-    "Rio do Sul",
-    "Resíduos Eletrônicos, Lâmpadas",
-  ];
+//   const values = [
+//     "https://www.ciaresiduos.com.br/wp-content/uploads/2016/03/images-4.jpg",
+//     "Colectoria",
+//     "Guilherme Gemballa, Jardim América",
+//     "Número 233",
+//     "Santa Catarina",
+//     "Rio do Sul",
+//     "Resíduos Eletrônicos, Lâmpadas",
+//   ];
 
-  function afterInsertData(err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("cadastrado com sucesso");
-    console.log(this);
-  }
+//   function afterInsertData(err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     console.log("cadastrado com sucesso");
+//     console.log(this);
+//   }
 
-  db.run(query, values, afterInsertData);
+//   db.run(query, values, afterInsertData);
 
-  // view data
+//   // view data
+//   db.all(`SELECT * FROM places`, function(err, row){
+//     if(err){
+//       return console.log(err)
+//     }
 
-  // delete data
-});
+//     console.log('Aqui estão seus registros')
+//     console.log(this)
+//   })
+
+
+//   // delete data
+
+//   db.run(`DELETE FROM places WHERE id = ?`, [?], function(err){
+//     if(err){
+//       return console.log(err)
+//     }
+//   })
+// });
+
+module.exports = db;
--- ecoleta/src/server.js
+++ ecoleta/src/server.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const server = express()
 
+// get db
+const db  = require("./database/db")
+
+
 // public folder
 server.use(express.static("public"))
+
+// support req.body
+server.use(express.urlencoded({extended: true}))
 
 //template engine nunjucks
 const nunjucks = require("nunjucks")
@@ -20,11 +27,78 @@ server.get('/', (req, res) =>{
 })
 // create point
 server.get('/create-point', (req, res) =>{
+
    res.render("create-point.html")
 })
+
+// POST create points
+server.post("/savepoint", (req, res) =>{
+
+   console.log(req.body)
+
+   // insert data into db
+   const query = `
+      INSERT INTO places (
+         image,
+         name,
+         address,
+         address2,
+         state,
+         city,
+         items
+
+      ) VALUES (?,?,?,?,?,?,?)
+   `;
+
+  const values = [
+     req.body.image,
+     req.body.name,
+     req.body.address,
+     req.body.address2,
+     req.body.state,
+     req.body.city,
+     req.body.items
+  ]
+
+  function afterInsertData(err) {
+    if (err) {
+      console.log(err);
+
+      return res.send("Erro no cadastro!")
+
+    }
+
+    console.log("cadastrado com sucesso");
+    console.log(this);
+    
+    return res.render("create-point.html", {saved: true})
+  }
+
+  db.run(query, values, afterInsertData);
+
+
+})
+
+
 // search results
 server.get('/search-results', (req, res) =>{
-   res.render("search-results.html")
+
+
+   db.all(`SELECT * FROM places`, function(err, row){
+          if(err){
+            return console.log(err)
+          }
+      
+          console.log('Aqui estão seus registros')
+          console.log(this)
+
+          const total = row.length;
+
+          //show html with db data
+          res.render("search-results.html", {places: row, total: total})
+        })
+
+
 })
 
 
